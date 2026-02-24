9554e59c40a88abd9098da54dde1ed92e810dac3
TeresaRuiz <glezyss411@gmail.com>
2026-02-24 01:18:51 -0600

    agregando nombre a los botones

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtNombre = new System.Windows.Forms.TextBox();
             this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnAtender = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -45,28 +45,12 @@
             this.groupBox1.Controls.Add(this.btnAgregar);
             this.groupBox1.Controls.Add(this.txtNombre);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(54, 40);
+            this.groupBox1.Location = new System.Drawing.Point(123, 52);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(726, 100);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Agregar cliente a la cola";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 48);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nombre del cliente";
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.Location = new System.Drawing.Point(188, 48);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(332, 26);
-            this.txtNombre.TabIndex = 1;
             // 
             // btnAgregar
             // 
@@ -77,50 +61,66 @@
             this.btnAgregar.Text = "Agregar a la cola";
             this.btnAgregar.UseVisualStyleBackColor = true;
             // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(188, 48);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(332, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del cliente";
+            // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.button3);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.button1);
-            this.groupBox2.Location = new System.Drawing.Point(54, 168);
+            this.groupBox2.Controls.Add(this.btnLimpiar);
+            this.groupBox2.Controls.Add(this.btnMostrar);
+            this.groupBox2.Controls.Add(this.btnAtender);
+            this.groupBox2.Location = new System.Drawing.Point(123, 180);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(726, 100);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Acciones";
             // 
-            // button1
+            // btnLimpiar
             // 
-            this.button1.Location = new System.Drawing.Point(83, 25);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 50);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Atender siguiente";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Location = new System.Drawing.Point(422, 25);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(117, 50);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar cola";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnMostrar
             // 
-            this.button2.Location = new System.Drawing.Point(252, 25);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(117, 50);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Mostrar lista de espera";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnMostrar.Location = new System.Drawing.Point(252, 25);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(117, 50);
+            this.btnMostrar.TabIndex = 4;
+            this.btnMostrar.Text = "Mostrar lista de espera";
+            this.btnMostrar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnAtender
             // 
-            this.button3.Location = new System.Drawing.Point(422, 25);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(117, 50);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Limpiar cola";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnAtender.Location = new System.Drawing.Point(83, 25);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(117, 50);
+            this.btnAtender.TabIndex = 3;
+            this.btnAtender.Text = "Atender siguiente";
+            this.btnAtender.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(846, 488);
+            this.ClientSize = new System.Drawing.Size(1014, 607);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -130,6 +130,7 @@
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
+
         }
 
         #endregion
@@ -139,9 +140,9 @@
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnAtender;
     }
 }
 
